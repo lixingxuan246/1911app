a94f44a16bf23c33e7197d567d15ffd2fcfbe155
lixingxuan <1171778420@qq.com>
2020-07-15 18:58:49 +0800

    .

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,10 +8,11 @@ class TestController extends Controller
 {
     //
     public function index(){
-        echo '123';
-        $appid = '';
-        $appsercret = '';
-        $url = '';
-        file_get_contents($url);
+//        echo '123';
+        $appid = 'wxfe4e16669b91d830';
+        $appsercret = 'b6cde8048ba58f021050069aafa613ea';
+        $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.$appid.'&secret='.$appsercret.'';
+        $a=file_get_contents($url);
+        echo $a;
     }
 }
